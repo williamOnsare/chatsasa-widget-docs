6e58d3003a00610dd0251dcb5984db9b0a566e3f
William Onsare <william.onsare254@gmail.com>
2023-07-31 16:43:24 +0300

    create a basic gatsby site for demo

---
+++ gatsby-site/gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
